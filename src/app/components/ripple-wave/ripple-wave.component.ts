7b3011b372f568f79b0719e25ccaafb8a85eef77
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-03-25 19:22:33 -0400

    create component & add route

---
+++ src/app/components/ripple-wave/ripple-wave.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ripple-wave',
+  imports: [],
+  templateUrl: './ripple-wave.component.html',
+  styleUrl: './ripple-wave.component.css'
+})
+export class RippleWaveComponent {
+
+}
