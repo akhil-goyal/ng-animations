50725e405fa6466d19b94026dbb305f577972598
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-03-25 19:01:37 -0400

    create component

---
+++ src/app/components/pulse-glow-button/pulse-glow-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pulse-glow-button',
+  imports: [],
+  templateUrl: './pulse-glow-button.component.html',
+  styleUrl: './pulse-glow-button.component.css'
+})
+export class PulseGlowButtonComponent {
+
+}
