9b9179405e1ff593a779626d75e59430ab66efc2
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-03-25 19:14:20 -0400

    create component & add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PulseGlowButtonComponent } from './components/pulse-glow-button/pulse-glow-button.component';
 import { SlideBounceCardComponent } from './components/slide-bounce-card/slide-bounce-card.component';
+import { FlipCardComponent } from './components/flip-card/flip-card.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'slide',
     component: SlideBounceCardComponent,
+  },
+  {
+    path: 'flip',
+    component: FlipCardComponent,
   },
 ];
