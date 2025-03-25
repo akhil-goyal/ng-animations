48ada30ecea29f571c1d92fec3830703c711f0f7
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-03-25 19:16:02 -0400

    create flip card animation

--- src/app/components/flip-card/flip-card.component.ts
+++ src/app/components/flip-card/flip-card.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-flip-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './flip-card.component.html',
-  styleUrl: './flip-card.component.css'
+  styleUrls: ['./flip-card.component.css'],
+  animations: [
+    trigger('flipCard', [
+      state(
+        'front',
+        style({
+          transform: 'perspective(1000px) rotateY(0deg)',
+        })
+      ),
+      state(
+        'back',
+        style({
+          transform: 'perspective(1000px) rotateY(180deg)',
+        })
+      ),
+      transition('front <=> back', [animate('0.6s ease-in-out')]),
+    ]),
+  ],
 })
 export class FlipCardComponent {
+  isFlipped = false;
 
+  flipCard() {
+    this.isFlipped = !this.isFlipped;
+  }
 }
