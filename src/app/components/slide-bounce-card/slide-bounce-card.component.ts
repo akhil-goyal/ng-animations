dcb1e90377d54330b006849fa41eba4dfca69010
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2025-03-25 19:11:13 -0400

    create component

---
+++ src/app/components/slide-bounce-card/slide-bounce-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-bounce-card',
+  imports: [],
+  templateUrl: './slide-bounce-card.component.html',
+  styleUrl: './slide-bounce-card.component.css'
+})
+export class SlideBounceCardComponent {
+
+}
